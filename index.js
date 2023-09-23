7486a29a5ef3dbfe50d9d46448ac00d55e885bbf
collangat@gmail.com <collangat@gmail.com>
2023-09-23 08:26:03 +0300

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <=400) {
+    return `This one is on me!`;
+  }
+  else if (feet > 2000 && feet <2500) {
+    return `I will gladly take your thirty bucks.`;
+  }
+  else if (feet >2500)
+   {
+    return `No can do.`;
+  }
+  //Good night!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let NYC;
+city = NYC ? 'Ok, sounds good.':`No go.`;
+return city;
+ // Write your code here!
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  let generous;
+switch (tip){
+  case `Thank you so much`:
+
+
+}`Thank you so much`
+
   // Write your code here!
 }
